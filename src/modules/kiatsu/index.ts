622ec3369b70265122e87bc0e95c8a000a87dd6a
nullnyat <nullnyat@outlook.com>
2022-02-07 15:25:59 +0900

    気圧機能追加

---
+++ src/modules/kiatsu/index.ts
@@ -0,0 +1,105 @@
+import autobind from "autobind-decorator"
+import fetch from "node-fetch"
+import { z } from "zod"
+
+import Module from "@/module"
+import Message from "@/message"
+import config from "@/config"
+
+export default class extends Module {
+	public readonly name = "kiatsu"
+
+	private readonly itemSchema = z.object({
+		time: z.enum(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]),
+		weather: z.string(),
+		temp: z.string(),
+		pressure: z.string(),
+		pressure_level: z.enum(["0", "1", "2", "3", "4"]),
+	})
+
+	private readonly schema = z.object({
+		place_name: z.literal("東京都中央区"),
+		place_id: z.literal("102"),
+		prefectures_id: z.literal("13"),
+		dateTime: z.string(),
+		yesterday: z.array(this.itemSchema),
+		today: z.array(this.itemSchema),
+		tomorrow: z.array(this.itemSchema),
+		dayaftertomorrow: z.array(this.itemSchema),
+	})
+
+	private currentPressureLevel: z.infer<typeof this.itemSchema>["pressure_level"] = "0"
+
+	private readonly stringPressureLevel: { [K in typeof this.currentPressureLevel]: string } = {
+		0: "問題ないかも。無理しないでね。",
+		1: "問題ないかも。無理しないでね。",
+		2: "ちょっとやばいかも。無理しないでね。",
+		3: "やばいかも。無理しないでね。",
+		4: "かなりやばいかも。無理しないでね。",
+	} as const
+
+	@autobind
+	public install() {
+		if (config.serverMonitoring) {
+			setInterval(this.update, 10 * 60 * 1000)
+			setInterval(this.post, 60 * 60 * 1000)
+
+			this.update()
+			this.post()
+		}
+
+		return {
+			mentionHook: this.mentionHook
+		}
+	}
+
+	@autobind
+	private async update() {
+		try {
+			const response = await fetch("https://zutool.jp/api/getweatherstatus/13102")
+			const data = await response.json()
+
+			const result = this.schema.safeParse(data)
+
+			if (!result.success) {
+				this.log("Validation failed.")
+				console.warn(result.error)
+
+				return
+			}
+
+			const date = new Date()
+			const hour = this.itemSchema.shape.time.parse(date.getHours().toString())
+
+			this.currentPressureLevel = result.data.today[hour].pressure_level
+
+		} catch (error) {
+			this.log("Failed to fetch status.")
+			console.warn(error)
+		}
+	}
+
+	@autobind
+	private post() {
+		if (this.currentPressureLevel === "0" || this.currentPressureLevel === "1") return
+
+		this.ai.post({
+			text: this.stringPressureLevel[this.currentPressureLevel]
+		})
+	}
+
+	@autobind
+	private async mentionHook(msg: Message) {
+		if (msg.includes(["気圧", "きあつ"])) {
+
+			msg.reply(this.stringPressureLevel[this.currentPressureLevel], {
+				immediate: true,
+			})
+			return true
+
+		} else {
+			return false
+		}
+	}
+}
+
