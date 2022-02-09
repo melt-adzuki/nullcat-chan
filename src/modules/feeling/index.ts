e2b0f978100a3d11d6019a93e206e1fcc77c97ef
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-02-09 23:49:18 +0900

    [fix] 不要なimportを削除しました

--- src/modules/feeling/index.ts
+++ src/modules/feeling/index.ts
@@ -1,9 +1,7 @@
 import autobind from 'autobind-decorator';
 import Module from '@/module';
 import Message from '@/message';
-import serifs from '@/serifs';
 import * as seedrandom from 'seedrandom';
-import { genItem } from '@/vocabulary';
 
 export const feelings = [
   'つらい',
