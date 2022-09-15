97bdceff5104ab49d959c98107647a72687c2cbb
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2022-09-15 18:13:29 +0900

    fix (ayashii): simplify the complex word to get mentioned

--- src/modules/ayashii/index.ts
+++ src/modules/ayashii/index.ts
@@ -15,8 +15,8 @@ export default class extends Module {
 
 	@autobind
 	private async mentionHook(message: Message) {
-		if (message.includes(['#怪しい日本語変換'])) {
-			const context = message.extractedText.replace('#怪しい日本語変換', '').trim();
+		if (message.includes(['#怪しい日本語'])) {
+			const context = message.extractedText.replace('#怪しい日本語', '').trim();
 			const cjp = generate(context);
 
 			message.reply(cjp + ' #怪レい曰本语');
